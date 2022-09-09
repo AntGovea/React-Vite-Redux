1adb409e58ea66fbec1b271468d1aebe2aee4d7f
Jose Antonio Govea Millan <teckblendxd@gmail.com>
2022-09-09 16:17:45 -0500

    increment y decrement

--- toolkit-redux/src/App.jsx
+++ toolkit-redux/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { descrement, increment } from './store/slices/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const {counter}=useSelector(state=>state.counter)
+  const dispatch=useDispatch();
+
+
+  //*obtener valores del store de redux
+  
 
   return (
     <div className="App">
@@ -17,16 +26,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={ ()=>dispatch( increment()  ) } >
+          Increment {counter}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={ ()=>dispatch( descrement()  ) } >
+          Descrement is {counter}
+        </button>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
